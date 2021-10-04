1a39fee5debf47f296e49ef8422e9f39006444c0
chandana540 <77038639+chandana540@users.noreply.github.com>
2021-10-04 18:43:45 +0530

    Add files via upload

---
+++ calculator/calculator.js
@@ -0,0 +1,15 @@
+function clr1()
+{
+    document.getElementById("result").value="";
+}
+function  display(val)
+{
+   document.getElementById("result").value+=val; 
+}
+function equate()
+{
+   let x= document.getElementById("result").value
+    let y=eval(x);
+    document.getElementById("result").value=y;
+
+}
